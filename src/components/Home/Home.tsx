183ed9b381caaebb753ce8e4461f34c406237028
Chris Durning <chrisdurning@Chriss-MacBook-Pro.local>
2023-04-19 13:10:35 +0100

    small fixes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,7 +3,7 @@ import Contact from "../Contact/Contact";
 import Experience from "../Experience/Experience";
 import Hero from "../Hero/Hero";
 import Projects from "../Projects/Projects";
-import Skills from "../Skills/Skills";
+// import Skills from "../Skills/Skills";
 
 interface HomeProps {}
 
