e5ff508d5a26f35dbf27a92b00a7a247954061e8
Chris Durning <chrisdurning@Chriss-MacBook-Pro.local>
2023-04-20 14:10:49 +0100

    small changes

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -19,8 +19,8 @@ const LightTooltip = styled(({ className, ...props }: TooltipProps) => (
   [`& .${tooltipClasses.tooltip}`]: {
     backgroundColor: theme.palette.common.white,
     color: 'rgba(0, 0, 0, 0.87)',
-    boxShadow: theme.shadows[1],
-    fontSize: 11,
+    fontSize: 10,
+    fontFamily: 'Cascadia'
   },
 }));
 
@@ -30,89 +30,89 @@ const Skills: FC<SkillsProps> = () => (
       <h3>Skills &#128295;</h3>
       <div className='skills-wrapper'>
         <IconContext.Provider value={{color: 'ivory', size: '40px'}}>
-          <div>
-            <h4>Languages</h4>
+          <div style={{width: '106px'}}>
+            <h4 style={{textAlign: 'center'}}>Languages</h4>
             <ul className='skills-list'>
-              <LightTooltip title="Javascript" placement="right-start">
+              <LightTooltip title="Javascript" placement="right">
                 <li><SiJavascript/></li>
               </LightTooltip>
-              <LightTooltip title="Typescript" placement="right-start">
+              <LightTooltip title="Typescript" placement="right">
                 <li><SiTypescript/></li>
               </LightTooltip>
-              <LightTooltip title="HTML" placement="right-start">
+              <LightTooltip title="HTML" placement="right">
                 <li><TiHtml5/></li>
                 </LightTooltip>
-              <LightTooltip title="CSS" placement="right-start">
+              <LightTooltip title="CSS" placement="right">
                 <li><DiCss3/></li>
               </LightTooltip>
-              <LightTooltip title="LESS" placement="right-start">
+              <LightTooltip title="LESS" placement="right">
                 <li><SiLess/></li>
               </LightTooltip>
-              <LightTooltip title="C#" placement="right-start">
+              <LightTooltip title="C#" placement="right">
                 <li><SiCsharp/></li>
               </LightTooltip>
-              <LightTooltip title="Java" placement="right-start">
+              <LightTooltip title="Java" placement="right">
                 <li><FaJava/></li>
               </LightTooltip>
-              <LightTooltip title="SQL" placement="right-start">
+              <LightTooltip title="SQL" placement="right">
                 <li><TbSql/></li>
               </LightTooltip>
-              <LightTooltip title="Python" placement="right-start">
+              <LightTooltip title="Python" placement="right">
                 <li><DiPython/></li>
               </LightTooltip>
             </ul>
           </div>
-          <div>
-            <h4>Frameworks</h4>
+          <div style={{width: '106px'}}>
+            <h4 style={{textAlign: 'center'}}>Frameworks</h4>
             <ul className='skills-list'>
-              <LightTooltip title="Angular" placement="right-start">
+              <LightTooltip title="Angular" placement="right">
                 <li><FaAngular/></li>
               </LightTooltip>
-              <LightTooltip title="ASP.NET Core & ASP.NET MVC" placement="right-start">
+              <LightTooltip title="ASP.NET Core & ASP.NET MVC" placement="right">
                 <li><SiDotnet/></li>
               </LightTooltip>
-              <LightTooltip title="React" placement="right-start">
+              <LightTooltip title="React" placement="right">
                 <li><FaReact/></li>
               </LightTooltip>
-              <LightTooltip title="Vue.js" placement="right-start">
+              <LightTooltip title="Vue.js" placement="right">
                 <li><FaVuejs/></li>
               </LightTooltip>
-              <LightTooltip title="Django & Django REST" placement="right-start">
+              <LightTooltip title="Django & Django REST" placement="right">
                 <li><DiDjango/></li>
               </LightTooltip>
-              <LightTooltip title="Node.js" placement="right-start">
+              <LightTooltip title="Node.js" placement="right">
                 <li><FaNode/></li>
               </LightTooltip>
-              <LightTooltip title="Jasmine" placement="right-start">
+              <LightTooltip title="Jasmine" placement="right">
                 <li><SiJasmine/></li>
               </LightTooltip>
             </ul>
           </div>
-          <div>
-            <h4>Tools</h4>
+          <div style={{width: '106px'}}>
+            <h4 style={{textAlign: 'center'}}>Tools</h4>
             <ul className='skills-list'>
-              <LightTooltip title="Git" placement="right-start">
+              <LightTooltip title="Git" placement="right">
                 <li><BsGithub/></li>
               </LightTooltip>
-              <LightTooltip title="Webpack" placement="right-start">
+              <LightTooltip title="Webpack" placement="right">
                 <li><SiWebpack/></li>
               </LightTooltip>
-              <LightTooltip title="Azure Dev Ops" placement="right-start">
+              <LightTooltip title="Azure Dev Ops" placement="right">
                 <li><SiAzuredevops/></li>
               </LightTooltip>
-              <LightTooltip title="Netlify" placement="right-start">
+              <LightTooltip title="Netlify" placement="right">
                 <li><SiNetlify/></li>
               </LightTooltip>
-              <LightTooltip title="Heroku" placement="right-start">
+              <LightTooltip title="Heroku" placement="right">
                 <li><SiHeroku/></li>
               </LightTooltip>
-              <LightTooltip title="Github Actions" placement="right-start">
+              <LightTooltip title="Github Actions" placement="right">
                 <li><SiGithubactions/></li>
               </LightTooltip>
-              <LightTooltip title="MySQL" placement="right-start">
+              <LightTooltip title="MySQL" placement="right">
                 <li><SiMysql/></li>
               </LightTooltip>
-              <LightTooltip title="PostgreSQL" placement="right-start">
+              <LightTooltip title="PostgreSQL" placement="right">
                 <li><SiPostgresql/></li>
               </LightTooltip>
             </ul>
